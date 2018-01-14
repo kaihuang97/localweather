5432e8eb14eca58e171f36f73ace3eef6c7aaca7
Kai Huang <kaihuang9397@gmail.com>
2018-01-14 15:03:45 -0800

    added wallpaper interactions

--- js/index.js
+++ js/index.js
@@ -15,9 +15,6 @@ $(document).ready(function () {
         var darkSkyAPI = darkSkyURL + location;
         $("#currentLocation").text(city + ", " + region);
         weather(darkSkyAPI);
-        var unsplashAPI = "https://api.unsplash.com/photos/random";
-        wallpaper(unsplashAPI);
-
     }
 
     function weather(darkSkyAPI) {
@@ -31,11 +28,16 @@ $(document).ready(function () {
         function getTemp(data) {
             var temp = Math.round(data.currently.temperature);
             var icon = data.currently.icon;
+            summary = data.currently.summary;
+            console.log(summary);
             $("#temperature").text(temp + "°F");
             getSkyCons(icon, temp);
+            var unsplashAPI = "https://api.unsplash.com/photos/random";
+            wallpaper(unsplashAPI);
         }
 
         function getSkyCons(icon, temp) {
+            /*
             var skycons = new Skycons({
                 "monochrome": false,
                 "colors": {
@@ -51,30 +53,34 @@ $(document).ready(function () {
                 }
             });
             skycons.add('skycon', icon);
+            */
+            var skycons = new Skycons({
+                "color": "white"
+            });
+            skycons.add("skycon", icon);
             skycons.play();
         }
-    }
 
-    function wallpaper(unsplashAPI) {
-        $.ajax({
-            headers: {
-                "Accept-Version": "v1",
-                "Authorization": "Client-ID 10c20ade5ab0257ce7ba5ad938efa572f6add11cec4f2ec4e45ecad170de8f53"
-            },
-            // using test 6084292b78479ead8972f469747ee02d056696a9f869ad5e4fcdaeddd7bd6ea7
-            type: "GET",
-            url: unsplashAPI,
-            data: {
-                count: 1,
-                query: "night"
-            },
-            success: getWallpaper
-        });
+        function wallpaper(unsplashAPI) {
+            $.ajax({
+                headers: {
+                    "Accept-Version": "v1",
+                    "Authorization": "Client-ID 10c20ade5ab0257ce7ba5ad938efa572f6add11cec4f2ec4e45ecad170de8f53"
+                },
+                // using test 6084292b78479ead8972f469747ee02d056696a9f869ad5e4fcdaeddd7bd6ea7
+                type: "GET",
+                url: unsplashAPI,
+                data: {
+                    count: 1,
+                    query: summary
+                },
+                success: getWallpaper
+            });
 
-        function getWallpaper(data) {
-            var image = data[0].urls.regular;
-            document.body.background = image;
-            console.log(data);
+            function getWallpaper(data) {
+                var image = data[0].urls.regular;
+                document.body.background = image;
+            }
         }
     }
 });
